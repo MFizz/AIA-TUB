bd68aee86e7fc847f48359b80104c61fa5561a59
Mitchfizz <Mitchfizz@Mitchfizz-PC.(none)>
2013-11-25 15:57:53 +0100

    aktueller stand

--- Assignment2/aia2.cpp
+++ Assignment2/aia2.cpp
@@ -56,17 +56,54 @@ int main(int argc, char** argv) {
 	getContourLine(exC1, contourLines1, binThreshold, numOfErosions);
 	getContourLine(exC2, contourLines2, binThreshold, numOfErosions);
 
+	// you could also reuse img1 here
+    Mat mask = Mat::zeros(exC2.rows, exC2.cols, CV_8UC1);
+
+	contourLines2.pop_back();
+
+    // CV_FILLED fills the connected components found
+    drawContours(mask, contourLines2, -1, Scalar(255), CV_FILLED);
+
 	// Mitja: just for showing preliminary results
 	showImage(exC1, "Result", 0);
 	showImage(exC2, "Result", 0);
-
+	showImage(mask, "Result", 0);
+	
 	// calculate fourier descriptor
 	Mat fd1 = makeFD(contourLines1.front());
 	Mat fd2 = makeFD(contourLines2.front());
 
+
+
+	Mat ch1, ch2;
+	// "channels" is a vector of 3 Mat arrays:
+	vector<Mat> channels(2);
+	// split img:
+	split(fd1, channels);
+	// get the channels (dont forget they follow BGR order in OpenCV)
+	ch1 = channels[0];
+	ch2 = channels[1];
+
+	//plotFD(fd1, "fd1");
+	cout << ch2.size() << " ch2 länge " << "\n";
+
+	cout << fd1.at<float>(0,1) << " 0 1 " << "\n";
+	cout << ch1.at<float>(0,0) << " ch1 0 0 " << "\n";
+	cout << ch2.at<float>(0,0) << " ch2 0 0 " << "\n";
+	cout << contourLines1.size() << " länge " << "\n";
+	cout << contourLines1.front().channels() << " channels " << "\n";
+	cout << fd1.channels() << " channels " << "\n";
+	cv::Size s = exC1.size();
+	cout << s.height << " Breite " << s.width << "\n";
+	s = fd1.size();
+	cout << s.height << " Breite " << s.width << "\n";
+	s = contourLines1.front().size();
+	cout << s.height << " Breite " << s.width << "\n";
+	s = contourLines1.back().size();
+	cout << s.height << " Breite " << s.width << "\n";	
 	//// normalize  fourier descriptor
 	//// TO DO !!!
-	////steps = ???;
+	//steps = 1;
 	//Mat fd1_norm = normFD(fd1, steps);
 	//Mat fd2_norm = normFD(fd2, steps);
 
@@ -81,8 +118,8 @@ int main(int argc, char** argv) {
 	//// get contour lines from image
 	//vector<Mat> contourLines;
 	//// TO DO !!!
-	////binThreshold = ???;
-	////numOfErosions = ???;
+	//binThreshold = 120;
+	//numOfErosions = 2;
 	//getContourLine(query, contourLines, binThreshold, numOfErosions);
 	//
 	//cout << "Found " << contourLines.size() << " object candidates" << endl;
@@ -98,7 +135,7 @@ int main(int argc, char** argv) {
 	//// loop through all contours found
 	//int i=1;
 	//// TO DO !!!
-	////detThreshold = ???;
+	//detThreshold = 120;
 	//for(vector<Mat>::iterator c = contourLines.begin(); c != contourLines.end(); c++, i++){
 
 	//    cout << "Checking object candidate no " << i << " :\t";
@@ -161,11 +198,38 @@ fd		the given fourier descriptor
 n		number of used frequencies (should be even)
 out		the normalized fourier descriptor
 */
-//Mat normFD(Mat& fd, int n){
-//  
-//  // TO DO !!!
-//  
-//}
+Mat normFD(Mat& fd, int n){
+	//vector<Mat> channels;
+	//split(fd, channels);
+ //   // translation 
+	//double t1 = -channels[0].at<float>(0,0)/fd.size().height;
+	//double t2 = -channels[1].at<float>(0,0)/fd.size().height;
+	//channels[0]+=fd.size().height * t1;
+	//channels[1]+=fd.size().height * t1;
+	//// scale 
+	//double f1 = abs(channels[0].at<float>(1,0));
+	//double f2 = abs(channels[1].at<float>(1,0));
+	//channels[0]/=f1;
+	//channels[1]/=f2;
+	//// rotation
+	//double f1 = abs(channels[0].at<float>(1,0));
+	//double f2 = abs(channels[1].at<float>(1,0));
+	//channels[0]/=f1;
+	//channels[1]/=f2;
+	//Mat tFD = Mat::zeros(fd.size(), CV_32F);
+	//merge(channels,2,tFD);
+	//fd.at<float>(0,0) = 0;
+	//fd.at<float>(0,1) = 0;
+
+	for(int i =0;i<2;++i) {
+		for(int j=0;j<fd.size().height;++j){
+			fd.at<float>(j,i) = abs(fd.at<float>(j,i)/abs(fd.at<float>(j,1)));
+		}
+	}
+
+	return fd;
+  
+}
 
 // calculates the (unnormalized) fourier descriptor from a list of points
 /*
@@ -173,9 +237,10 @@ contour		1xN 2-channel matrix, containing N points (x in first, y in second chan
 out		fourier descriptor (not normalized)
 */
 Mat makeFD(Mat& contour){
+
 	Mat floatContour;
 	contour.convertTo(floatContour,CV_32F);
-	dft(floatContour,floatContour,DFT_ROWS); 
+	dft(floatContour,floatContour); 
 	return floatContour;
 }
 
@@ -187,9 +252,10 @@ thresh		threshold used to binarize the image
 k		number of applications of the erosion operator
 */
 void getContourLine(Mat& img, vector<Mat>& objList, int thresh, int k){
+
 	threshold(img, img, thresh, 255,THRESH_BINARY);
 	dilate(img,img,Mat(), Point(-1, -1), k);
-	cv::findContours(img,objList,CV_RETR_TREE,CV_CHAIN_APPROX_SIMPLE);
+	cv::findContours(img,objList,CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
 }
 
 // plot fourier descriptor
@@ -198,11 +264,15 @@ fd	the fourier descriptor to be displayed
 win	the window name
 dur	wait number of ms or until key is pressed
 */
-//void plotFD(Mat& fd, string win, double dur){
-//
-//    // TO DO !!!
-//    
-//}
+void plotFD(Mat& fd, string win, double dur){
+	// use copy for normalization
+    Mat tempDisplay = fd.clone();
+    if (fd.channels() == 1) normalize(fd, tempDisplay, 0, 255, CV_MINMAX);
+    namedWindow( win.c_str(), CV_WINDOW_AUTOSIZE );
+    imshow( win.c_str(), tempDisplay );
+    // wait
+    if (dur>=0) waitKey(dur);
+}
 
 // shows the image
 /*
